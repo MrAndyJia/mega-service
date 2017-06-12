18572776f4f026d683e67cbda156a80aadfcb676
MrAndyJia <jiazx0107@163.com>
2017-06-12 22:59:30 +0800

    提交测试代码

---
+++ src/main/java/com/mega/service/TestTcpClient.java
@@ -0,0 +1,49 @@
+package com.mega.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TestTcpClient {
+
+	public static void main(String[] args) {
+		try {
+			// 创建Socket对象
+			Socket socket = new Socket("127.0.0.1", 8888);
+
+			// 根据输入输出流和服务端连接
+			OutputStream outputStream = socket.getOutputStream();// 获取一个输出流，向服务端发送信息
+			PrintWriter printWriter = new PrintWriter(outputStream);// 将输出流包装成打印流
+			printWriter.print("201,0,1,2,3,4,5,6,7,4,3,2,24,2,3,5,2");
+			printWriter.flush();
+			socket.shutdownOutput();// 关闭输出流
+
+			InputStream inputStream = socket.getInputStream();// 获取一个输入流，接收服务端的信息
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);// 包装成字符流，提高效率
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);// 缓冲区
+			String info = "";
+			String temp = null;// 临时变量
+			while ((temp = bufferedReader.readLine()) != null) {
+				info += temp;
+				System.out.println("客户端接收服务端发送信息：" + info);
+			}
+
+			// 关闭相对应的资源
+			bufferedReader.close();
+			inputStream.close();
+			printWriter.close();
+			outputStream.close();
+			socket.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/main/java/com/mega/service/common/Constant.java
@@ -0,0 +1,21 @@
+package com.mega.service.common;
+
+/**
+ * 
+ * @author Andy
+ *
+ */
+public class Constant {
+	
+	
+	public static final int SOCKET_PORT = 8888;
+	
+	
+	public static final String SOCKET_RESULT_SUCCESS = "1";
+	
+	public static final String SOCKET_RESULT_ERROR = "0";
+	
+	
+	
+
+}
